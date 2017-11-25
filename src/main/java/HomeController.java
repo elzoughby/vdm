514bfab3355538687688902cb6e3c06c93c12881
Ahmed Elzoughby <gs_elzoughby@yahoo.com>
2017-11-25 15:37:10 +0200

    finishing Add to Queue and Remove from Queue options

--- src/main/java/HomeController.java
+++ src/main/java/HomeController.java
@@ -1,4 +1,3 @@
-import javafx.application.Platform;
 import javafx.beans.binding.Bindings;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -9,18 +8,15 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.MenuItem;
-import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.*;
 import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
-import java.awt.*;
+import java.awt.Desktop;
 import java.io.File;
 import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
@@ -31,53 +27,22 @@ public class HomeController implements Initializable {
     private static ObservableList<Item> itemList = FXCollections.observableArrayList();
     private static ObservableList<Item> queueItemList = FXCollections.observableArrayList();
 
-
     @FXML
     private BorderPane homeWindowPane;
     @FXML
     private SplitPane homeSplitPane;
     @FXML
+    private ToggleButton queueBtn;
+    @FXML
     private ListView<String> consoleListView;
     @FXML
     private TableView<Item> itemsTableView;
-    @FXML
-    private TableColumn<Item, Integer> itemsIdColumn;
-    @FXML
-    private TableColumn<Item, String> itemsTitleColumn;
-    @FXML
-    private TableColumn<Item, String> itemsStatusColumn;
-    @FXML
-    private TableColumn<Item, String> itemsSizeColumn;
-    @FXML
-    private TableColumn<Item, String> itemsSpeedColumn;
-    @FXML
-    private TableColumn<Item, String> itemsDoneColumn;
-    @FXML
-    private TableColumn<Item, String> itemsEtaColumn;
 
 
-
-
-    public static List<Item> getItemList() {
-        return itemList;
-    }
-
-    public static List<Item> getQueueItemList() {
-        return queueItemList;
-    }
 
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
-        // binding table columns data with items attributes
-        itemsIdColumn.setCellValueFactory(new PropertyValueFactory<>("itemId"));
-        itemsTitleColumn.setCellValueFactory(new PropertyValueFactory<>("title"));
-        itemsStatusColumn.setCellValueFactory(new PropertyValueFactory<>("status"));
-        itemsSizeColumn.setCellValueFactory(new PropertyValueFactory<>("sizeString"));
-        itemsSpeedColumn.setCellValueFactory(new PropertyValueFactory<>("speedString"));
-        itemsDoneColumn.setCellValueFactory(new PropertyValueFactory<>("doneString"));
-        itemsEtaColumn.setCellValueFactory(new PropertyValueFactory<>("eta"));
 
         // filling the table with download items
         itemsTableView.setItems(itemList);
@@ -91,6 +56,12 @@ public class HomeController implements Initializable {
         });
 
         // context menu construction
+        drawItemsContextMenu();
+
+    }
+
+    private void drawItemsContextMenu() {
+
         ContextMenu rowContextMenu = new ContextMenu();
 
         MenuItem startMenuItem = new MenuItem("Start");
@@ -105,9 +76,13 @@ public class HomeController implements Initializable {
         deleteMenuItem.setOnAction(event -> removeBtnAction());
         deleteMenuItem.setGraphic(new ImageView(new Image(getClass().getResource("menu/delete.png").toString())));
 
-        MenuItem queueMenuItem = new MenuItem("Add to queue");
-        queueMenuItem.setOnAction(event -> addToQueueMenuAction());
-        queueMenuItem.setGraphic(new ImageView(new Image(getClass().getResource("menu/queue.png").toString())));
+        MenuItem addToQueueMenuItem = new MenuItem("Add to Queue");
+        addToQueueMenuItem.setOnAction(event -> addToQueueMenuAction());
+        addToQueueMenuItem.setGraphic(new ImageView(new Image(getClass().getResource("menu/queue.png").toString())));
+
+        MenuItem removeFromQueueMenuItem = new MenuItem("Remove from Queue");
+        removeFromQueueMenuItem.setOnAction(event -> removeFromQueueMenuAction());
+        removeFromQueueMenuItem.setGraphic(new ImageView(new Image(getClass().getResource("menu/queue.png").toString())));
 
         MenuItem clearMenuItem = new MenuItem("Clear Logs");
         clearMenuItem.setOnAction(event -> clearLogsMenuAction());
@@ -122,19 +97,91 @@ public class HomeController implements Initializable {
         infoMenuItem.setGraphic(new ImageView(new Image(getClass().getResource("menu/details.png").toString())));
 
         rowContextMenu.getItems().addAll(startMenuItem, pauseMenuItem, deleteMenuItem,
-                queueMenuItem, clearMenuItem, openFolderMenuItem, infoMenuItem);
+                addToQueueMenuItem, clearMenuItem, openFolderMenuItem, infoMenuItem);
 
-        // show context menu for not null rows only
+
+
         itemsTableView.setRowFactory(param -> {
 
             TableRow<Item> row = new TableRow<>();
+
+            // change context menu queue option with selecting queueBtn
+            queueBtn.selectedProperty().addListener((observable, oldValue, newValue) -> {
+                if(newValue)
+                    rowContextMenu.getItems().set(3, removeFromQueueMenuItem);
+                else
+                    rowContextMenu.getItems().set(3, addToQueueMenuItem);
+            });
+
+            // show context menu for not null rows only
             row.contextMenuProperty().bind(Bindings.when(Bindings.isNotNull(row.itemProperty()))
-                            .then(rowContextMenu)
-                            .otherwise((ContextMenu) null));
+                    .then(rowContextMenu)
+                    .otherwise((ContextMenu) null));
             return row;
         });
 
     }
+
+    static List<Item> getItemList() {
+        return itemList;
+    }
+
+    static List<Item> getQueueItemList() {
+        return queueItemList;
+    }
+
+
+    private void addToQueueMenuAction() {
+
+        Item selectedItem = itemsTableView.getSelectionModel().getSelectedItem();
+        selectedItem.setAddToQueue(true);
+        queueItemList.add(selectedItem);
+        itemList.remove(selectedItem);
+
+    }
+
+    private void removeFromQueueMenuAction() {
+
+        Item selectedItem = itemsTableView.getSelectionModel().getSelectedItem();
+        selectedItem.setAddToQueue(false);
+        itemList.add(selectedItem);
+        queueItemList.remove(selectedItem);
+
+    }
+
+    private void clearLogsMenuAction() {
+
+        itemsTableView.getSelectionModel().getSelectedItem().getLogList().clear();
+
+    }
+
+    private void openFolderMenuAction() {
+
+        new Runnable() {
+
+            @Override
+            public void run() {
+
+                try {
+
+                    String location = itemsTableView.getSelectionModel().getSelectedItem().getLocation();
+                    Desktop desktop = null;
+                    File file = new File(location);
+                    if (Desktop.isDesktopSupported())
+                        desktop = Desktop.getDesktop();
+                    if (desktop != null)
+                        desktop.open(file);
+
+                } catch (IOException e) {
+                    System.err.println("Error Opening Location Folder");
+                }
+
+            }
+
+        };
+
+    }
+
 
     @FXML
     void addBtnAction() {
@@ -265,48 +312,6 @@ public class HomeController implements Initializable {
             homeSplitPane.getItems().add(1, consoleListView);
             homeSplitPane.setDividerPosition(0, 0.7);
         }
-
-    }
-
-    @FXML
-    void addToQueueMenuAction() {
-
-        //itemsTableView.getSelectionModel().getSelectedItem().getLogList().clear();
-
-    }
-
-    @FXML
-    void clearLogsMenuAction() {
-
-        itemsTableView.getSelectionModel().getSelectedItem().getLogList().clear();
-
-    }
-
-    @FXML
-    void openFolderMenuAction() {
-
-        new Runnable() {
-
-            @Override
-            public void run() {
-
-                try {
-
-                    String location = itemsTableView.getSelectionModel().getSelectedItem().getLocation();
-                    Desktop desktop = null;
-                    File file = new File(location);
-                    if (Desktop.isDesktopSupported())
-                        desktop = Desktop.getDesktop();
-                    if (desktop != null)
-                        desktop.open(file);
-
-                } catch (IOException e) {
-                    System.err.println("Error Opening Location Folder");
-                }
-
-            }
-
-        };
 
     }
 
