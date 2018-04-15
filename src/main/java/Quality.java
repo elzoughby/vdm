418f31b06f746dceedc6d5ee967e13da77a90e9d
Ahmed Elzoughby <gs_elzoughby@yahoo.com>
2018-04-15 21:20:15 +0200

    filling the quality class

--- src/main/java/Quality.java
+++ src/main/java/Quality.java
@@ -1,9 +1,56 @@
 public class Quality {
 
+    public enum Type {
+        FULL_VIDEO,
+        VIDEO_ONLY,
+        AUDIO_ONLY
+    }
+
     private String code;
     private String extension;
     private String resolution;
+    private Type type;
+    private String note;
 
 
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getExtension() {
+        return extension;
+    }
+
+    public void setExtension(String extension) {
+        this.extension = extension;
+    }
+
+    public String getResolution() {
+        return resolution;
+    }
+
+    public void setResolution(String resolution) {
+        this.resolution = resolution;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
 
 }
